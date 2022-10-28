9bf32a81086e4c62fd3f1584996552ea26ca0054
Brahim <bra-95@live.fr>
2022-10-29 00:15:33 +0100

    first db fetch and props passing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,30 @@
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import prisma from "../lib/prisma";
+import { PhotosData } from "../types/prisma";
 
-const Home: NextPage = () => {
-  return <div> dfsf </div>;
+const Home: NextPage<Record<string, PhotosData[]>> = ({ feed }) => {
+  return (
+    <div>
+      {feed.map((photo, i) => (
+        <img key={i} src={photo.url}></img>
+      ))}
+    </div>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const feed = await prisma.photo.findMany({
+    include: {
+      owner: {
+        select: { name: true }
+      }
+    }
+  });
+  return {
+    props: { feed }
+  };
 };
 
 export default Home;
