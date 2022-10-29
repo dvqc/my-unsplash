764c6463addbd6baa693e113f85e904be2ba52a0
Brahim <bra-95@live.fr>
2022-10-30 00:19:59 +0100

    adding next auth with google provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
 import "../styles/globals.scss";
+import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  <SessionProvider session={session}>
+    <Component {...pageProps} />
+  </SessionProvider>;
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,29 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import prisma from "../lib/prisma";
 import { PhotosData } from "../types/prisma";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 const Home: NextPage<Record<string, PhotosData[]>> = ({ feed }) => {
+  const { data: session } = useSession();
+  console.log(session);
+  if (session) {
+    return (
+      <>
+        Signed in as {session?.user?.email} <br />
+        <div>
+          {feed.map((photo, i) => (
+            <img key={i} src={photo.url}></img>
+          ))}
+        </div>
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    );
+  }
   return (
-    <div>
-      {feed.map((photo, i) => (
-        <img key={i} src={photo.url}></img>
-      ))}
-    </div>
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
   );
 };
 
