23a958f26619d883fa38b08c51906c5c501aac5f
dvqc-office <bra-95@live.fr>
2022-11-06 10:05:35 +0100

    added a genreic usePagination hook

---
+++ hooks/usePagination.ts
@@ -0,0 +1,33 @@
+import { Photo } from "@prisma/client";
+import useSWRInfinite from "swr/infinite";
+
+const usePagination = <T>(url: string, pageSize: number) => {
+  const getKey = (pageIndex: number, previousPageData: any) => {
+    if (previousPageData && !previousPageData.length) return null;
+    return `${url}/?skip=${pageIndex * pageSize}&take=${pageSize}`;
+  };
+
+  const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+  const { data, error, setSize, size } = useSWRInfinite<T[]>(getKey, fetcher);
+
+  const isLoadingInitialData = !data && !error;
+  const isLoadingMore =
+    isLoadingInitialData ||
+    (size > 0 && data && typeof data[size - 1] === "undefined");
+  const isEmpty = data?.[0]?.length === 0;
+  const isReachingEnd =
+    isEmpty || (data && data[data.length - 1]?.length < pageSize);
+
+  return {
+    data,
+    isLoadingInitialData,
+    isLoadingMore,
+    isEmpty,
+    isReachingEnd,
+    setSize,
+    size
+  };
+};
+
+export default usePagination;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,16 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import { useSession } from "next-auth/react";
 import { PhotosContainer } from "../components/Photo";
-import React, { createRef, useEffect, useState } from "react";
-import useSWRInfinite from "swr/infinite";
+import React, { createRef, useState } from "react";
 import Loader from "../components/Loader";
 import InfiniteScroll from "react-infinite-scroll-component";
 import Separator from "../components/Separator";
 import { DeleteModal, AddModal } from "../components/Modal";
-import DefaultHeader, { Left, Header } from "../components/Header";
+import DefaultHeader from "../components/Header";
 import Signin from "../components/Signin";
 import Empty from "../components/Empty";
-
-const PAGE_SIZE = 7;
+import { usePagination } from "hooks";
+import { Photo } from "@prisma/client";
 
 const Home: NextPage = () => {
   const { data: session, status } = useSession();
@@ -21,22 +20,12 @@ const Home: NextPage = () => {
   const addModalRef = createRef<HTMLDialogElement>();
   const deleteModalRef = createRef<HTMLDialogElement>();
 
-  const getKey = (pageIndex: number, previousPageData: any) => {
-    if (previousPageData && !previousPageData.length) return null;
-    return `/api/photos/?skip=${pageIndex * PAGE_SIZE}&take=${PAGE_SIZE}`;
-  };
+  const PAGE_SIZE = 7;
 
-  const fetcher = (url: string) => fetch(url).then((res) => res.json());
-
-  const { data, error, setSize, size } = useSWRInfinite(getKey, fetcher);
-
-  const isLoadingInitialData = !data && !error;
-  const isLoadingMore =
-    isLoadingInitialData ||
-    (size > 0 && data && typeof data[size - 1] === "undefined");
-  const isEmpty = data?.[0]?.length === 0;
-  const isReachingEnd =
-    isEmpty || (data && data[data.length - 1]?.length < PAGE_SIZE);
+  const { data, isEmpty, isReachingEnd, setSize, size } = usePagination<Photo>(
+    "api/photos",
+    PAGE_SIZE
+  );
 
   if (status == "loading") return <Loader />;
 
